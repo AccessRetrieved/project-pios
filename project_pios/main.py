629010528ce0a3295cc185eb0500a25392d7237d
Jerry Hu <work.jerrywu@gmail.com>
2021-02-02 02:24:03 -0800

    版本4.0.8更新

--- project_pios/main.py
+++ project_pios/main.py
@@ -52,7 +52,7 @@ NSLocalVersion = StringVar()                  #
 #                                             #
 # U P D A T E   T H I S   E V E R Y T I M E ! #
 #                                             #
-NSLocalVersion.set('4.0.7')                   #
+NSLocalVersion.set('4.0.8')                   #
 ###############################################
 ##################################
 #                                #
